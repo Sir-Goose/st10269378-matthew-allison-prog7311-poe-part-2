8addb6068a49a02027a1b0579c7b29fe20ffb395
Matthew Allison <matthewallisonxyz@gmail.com>
2025-05-12 17:53:36 +0200

    Comments!

--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
 using prog7311.Repository;
 using prog7311.Services;
 
+/*
+ * This is the Program.cs file. It is the entry point of the program. It is where all of the dependencies
+ * are injected. A bunch of MVC Asp Net framework options are set and the app runs.
+ */
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -22,11 +27,16 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
+// Automatically redirect http requests to https.
+app.UseHttpsRedirection(); 
+
+// Enable the built in routing to match incoming requests to the correct endpoints.
 app.UseRouting();
 
+// Enable the built in authorisation middleware.
 app.UseAuthorization();
 
+// Map the static assets such as images to be served by the web server
 app.MapStaticAssets();
 
 app.MapControllerRoute(
@@ -34,4 +44,5 @@ app.MapControllerRoute(
         pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
+// Run the app.
 app.Run();
