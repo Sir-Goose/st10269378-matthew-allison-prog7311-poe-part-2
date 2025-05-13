33bd40110ab6d7fdb97ab8bc2aa631754d8fac8a
Matthew Allison <matthewallisonxyz@gmail.com>
2025-05-13 16:02:36 +0200

    Add comments and documentation to AppDbContext class

--- Repository/AppDbContext.cs
+++ Repository/AppDbContext.cs
@@ -3,36 +3,50 @@ using prog7311.Models;
 using System;
 using System.Linq;
 
+/*
+ * This is the AppDbContext class. It is responsible for the initial creation
+ * and seeding of data into the database. It uses entity framework.
+ */
 namespace prog7311.Repository
 {
     public class AppDbContext : DbContext
     {
+        // farmers table
         public DbSet<Farmer> Farmers { get; set; }
+        // employee table
         public DbSet<Employee> Employees { get; set; }
+        // products table
         public DbSet<Product> Products { get; set; }
 
+        // Configure the database connection to the local sqlite database called app.db
         protected override void OnConfiguring(DbContextOptionsBuilder options)
             => options.UseSqlite("Data Source=app.db");
 
+        // Primary constructor
         public AppDbContext()
         {
+            // Make sure the database and tables exist
             Database.EnsureCreated();
+            // Add in the default data into the database
             SeedDatabase();
         }
 
+        // This is the seed database method. It creates and adds employees farmers and products 
+        // as well as assigning products to farmers
         private void SeedDatabase()
         {
+            // Only run if database is not empty 
             if (!Employees.Any() && !Farmers.Any() && !Products.Any())
             {
-                // Employees
+                // Seed Employees
                 Employees.AddRange(new[]
                 {
                     new Employee { Name = "Clive Frankland", Email = "clive.frankland@example.com", Password = "password1" },
-                    new Employee { Name = "Matthew Allison", Email = "matthew.alison@example.com", Password = "password2" }
+                    new Employee { Name = "Matthew Allison", Email = "matthew.allison@example.com", Password = "password2" }
                 });
                 SaveChanges();
 
-                // Farmers
+                // Seed Farmers
                 var farmers = new[]
                 {
                     new Farmer { Name = "Joshua Shields", Email = "joshua.shields@example.com", Password = "boer123" },
@@ -58,6 +72,8 @@ namespace prog7311.Repository
                     ("Granadillas", "Fruit"),
                     ("Butternut", "Vegetable")
                 };
+                
+                // Randomly distribute products amongst the farmers. 5 each
                 var rand = new Random();
                 foreach (var farmer in Farmers)
                 {
